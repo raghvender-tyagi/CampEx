dbe29edf088396ec7139301d8f8ecd37b58a00fd
raghvender-tyagi <sunilkumartyagi900@gmail.com>
2026-06-22 00:38:25 +0530

    fix: allow django to serve media files in production on Render

--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,9 @@ urlpatterns = [
     path('', include('listings.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
